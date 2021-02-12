1685f44f1a9ea8734863f22d6ae4e2a0daa199d3
deepakviswanadh <deepak16c@gmail.com>
2021-02-12 10:47:47 +0530

    nav bar added

--- src/App.js
+++ src/App.js
@@ -1,43 +1,65 @@
 import React, { Fragment, useState } from "react";
-//import Applet from "./components/Applet";
-//import Search from "./components/Search";
-//import Dropdown from "./components/Dropdown";
+import Applet from "./components/Applet";
+import Search from "./components/Search";
+import Dropdown from "./components/Dropdown";
 import Translate from "./components/Translate/Translate";
+import Header from "./components/Header";
+import Route from "./components/Route";
+
 const App = () => {
-  // const quotes = [
-  //   { title: "t1", content: "d1" },
-  //   { title: "t2", content: "d2" },
-  // ];
+  const quotes = [
+    {
+      title: "What is React?",
+      content: "React is a front end javascript framework",
+    },
+    {
+      title: "Why use React?",
+      content: "React is a favorite JS library among engineers",
+    },
+    {
+      title: "How do you use React?",
+      content: "You use React by creating components",
+    },
+  ];
 
-  // const options = [
-  //   {
-  //     label: "Red Color",
-  //     value: "red",
-  //   },
-  //   {
-  //     label: "Blue Color",
-  //     value: "blue",
-  //   },
-  //   {
-  //     label: "Green color",
-  //     value: "green",
-  //   },
-  // ];
+  const options = [
+    {
+      label: "Red Color",
+      value: "red",
+    },
+    {
+      label: "Blue Color",
+      value: "blue",
+    },
+    {
+      label: "Green color",
+      value: "green",
+    },
+  ];
 
-  // //default value for dropdown is set to red
-  // const [selected, setSelected] = useState(options[0]);
+  //default value for dropdown is set to red
+  const [selected, setSelected] = useState(options[0]);
 
   return (
     <Fragment>
-      {/* <Applet text={quotes} />
-      <Search /> */}
-      {/* <Dropdown
-      label="select a color"
-        selected={selected}
-        onSelectedHandler={setSelected}
-        choice={options}
-      /> */}
-      <Translate />
+      <Header />
+      <Route path="/">
+        <Applet text={quotes} />
+      </Route>
+      <Route path="/list">
+        <Search />
+      </Route>
+      <Route path="/dropdown">
+        <Dropdown
+          label="Select a color"
+          selected={selected}
+          choice={options}
+          onSelectedHandler={setSelected}
+        />
+      </Route>
+      <Route path="/translate">
+        <Translate />
+      </Route>
     </Fragment>
   );
 };
